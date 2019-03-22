7dd4dcd72efe96e9d01bd97b681c9e3bb80901f4
166660614 <166660614@qq.com>
2019-03-22 10:38:57 +0800

    fsd

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::get('/weixin/api', 'Api\ApiController@test');
 Route::get('/weixin/menu', 'Weixin\WeixinController@viewMenu');
 Route::post('/weixin/passmenu', 'Weixin\WeixinController@passMenu');
 
-Route::post('/u/ulogin', 'User\LoginRegister@ulogn');//App登录逻辑
+Route::post('/u/ulogin', 'User\LoginRegister@ulogin');//App登录逻辑
 Route::post('/u/register', 'User\LoginRegister@uregister');//App注册逻辑
 
